70c5d8b745c8bac813d39ddc9c9705bd82ebd269
Riki <riki.cmd@gmail.com>
2022-10-25 22:09:22 +0700

    Robith Rivaldy Flutter Firebase Foodsapp #4

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,11 +8,109 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-            child: Image.asset(
-          'assets/pic1.png',
-          width: 100,
-        )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 32,
+            horizontal: 20,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello, Robith',
+                style: poppinsTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: blackColor),
+              ),
+              Text(
+                'Selamat datang di Burger Jawa',
+                style: poppinsTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: greyColor),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Recomended Menu',
+                style: poppinsTextStyle.copyWith(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: blackColor,
+                ),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/pic1.png',
+                    width: 130,
+                    height: 110,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/promo.png',
+                        width: 47,
+                        height: 16,
+                      ),
+                      Text(
+                        'Burger Reguler',
+                        style: poppinsTextStyle.copyWith(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: blackColor,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'Rp 12.000',
+                            style: poppinsTextStyle.copyWith(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: greyColor,
+                                decoration: TextDecoration.lineThrough),
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Text(
+                            'Rp 12.000',
+                            style: poppinsTextStyle.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14,
+                              color: yellowColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        'Free Delivery',
+                        style: poppinsTextStyle.copyWith(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12,
+                          color: greyWhiteColor,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,5 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 Color blackColor = const Color(0xff000000);
 Color greyColor = const Color(0xffAAAAAA);
 Color yellowColor = const Color(0xffEDBE16);
+Color greyWhiteColor = const Color(0xff7A7E86);
 
 TextStyle poppinsTextStyle = GoogleFonts.poppins();
